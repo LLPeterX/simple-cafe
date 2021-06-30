aff8c95d1d81e2cc3f6a95fea6f2e893754524f8
Peter Lebedev <LPeter@mail.ru>
2021-06-30 12:52:53 +0300

    user registration and login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,12 +1,57 @@
 const ApiError = require("../errors/apiError");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User, Backet } = require('../models/models');
+
+// генерация токена авторизации
+const generateToken = (id, email, role) => {
+  return jwt.sign(
+    { id, email, role },
+    process.env.SECRET_KEY,
+    { expiresIn: '24h' }
+  );
+};
 
 class UserController {
-  async registration(req, res) {
-
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password || password.length < 3 || email.length < 3) {
+      return next(ApiError.badRequest("Некорректные имя или пароль"));
+    }
+    // проверяем, что пользователь уже существует (по email)
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest("Такой пользователь уже существует"));
+    }
+    // пользователя с такм email не нашли - создаем нового
+    const encryptedPassword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, role, password: encryptedPassword });
+    // сразу создам корзину для пользователя
+    await Backet.create({ userId: user.id });
+    // создаем токен авторизации
+    // const token = jwt.sign(
+    //   { id: user.id, email: user.email, role },
+    //   process.env.SECRET_KEY,
+    //   { expiresIn: '24h' }
+    // );
+    const token = generateToken(user.id, user.email, user.role);
+    return res.json(token);
   }
 
-  async login(req, res) {
-    res.json({ message: 'Login request' });
+  async login(req, res, next) {
+    const { email, password } = req.body;
+    // проверяем, что пользователь с указанным email есть в базе
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.forbidden('Пользователь не найден'));
+    }
+    // сравниваем пароли
+    const cmpPassword = bcrypt.compareSync(password, user.password);
+    if (!cmpPassword) {
+      return next(ApiError.forbidden('Неверный пароль'));
+    }
+    // генерируем и возвращаем новый токен
+    return res.json(generateToken(user.id, user.email, user.role));
   }
 
   async checkAuth(req, res, next) {
@@ -14,7 +59,7 @@ class UserController {
     if (!id) {
       return next(ApiError.badRequest('Пользователь не залогинен'));
     }
-    res.json({ message: `Юзер зашел под ${id}` });
+    return res.json({ message: `Юзер зашел под ${id}` });
   }
 }
 
