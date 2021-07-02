9a03c08c0cb6f31d1a5e61fe20166cdd4d38f770
Peter Lebedev <LPeter@mail.ru>
2021-07-02 12:06:19 +0300

    UserStore and context

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -3,14 +3,17 @@ AppRouter: Навигация по страницам из pages
 */
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { Context } from '..';
 import { authRoutes, publicRoutes } from '../routes';
 import { SHOP_ROUTE } from '../utils/constants';
 
 const AppRouter = () => {
-  const isAuth = true;
+  //const isAuth = true;
+  const { user } = React.useContext(Context);
+  console.log(user);
   return (
     <Switch>
-      {isAuth &&
+      {user.isAuth &&
         authRoutes.map(({ path, Component }) => <Route exact key={path} path={path} component={Component} />)
       }
       {
