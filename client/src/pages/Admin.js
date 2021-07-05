38abec72f4fe1dbc313543e03e292d7b9e75a7f6
Peter Lebedev <LPeter@mail.ru>
2021-07-05 13:33:23 +0300

    hide/show modal windows

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,19 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Container, Row, Col, Button, Table } from 'react-bootstrap'
 import EditType from '../components/modals/EditType';
 import EditProduct from '../components/modals/EditProduct';
 import TypeBar from '../components/TypeBar';
 import { Context } from "../index"
 
-
 const Admin = () => {
   const { product } = useContext(Context);
+  const [typeModalVisible, setTypeModalVisible] = useState(false);
+  const [productModalVisible, setProductModalVisible] = useState(false);
+  const hideTypeModal = () => setTypeModalVisible(false);
+  const hideProductModal = () => setProductModalVisible(false);
   return (
     <Container>
-      <Row className="mt-2">
+      <Row className="mt-2"></Row>
         <Col md={3}>
           <TypeBar />
-          <Button className="mt-2">Добавить тип продукта</Button>
+          <Button className="mt-2" onClick={() => setTypeModalVisible(true)}>Добавить тип продукта</Button>
         </Col>
         <Col md={9}>
           {/* Тут список товаров без картинок */}
@@ -47,12 +50,12 @@ const Admin = () => {
               }
             </tbody>
           </Table>
-          <Button className="mt-2 align-self-end">Сохранить</Button>
+          <Button className="mt-2 align-self-end" onClick={() => setProductModalVisible(true)}>Сохранить</Button>
         </Col>
       </Row>
-      <EditType show={true} />
-      <EditProduct />
-    </Container>
+      <EditType show={typeModalVisible} onHide={hideTypeModal} />
+      <EditProduct show={productModalVisible} onHide={hideProductModal} />
+    </Container >
   );
 };
 
